3c38f120fd2cf08ab4b093426e35826c82c37017
riantkb <kubota.rian@gmail.com>
2019-05-28 19:08:04 +0900

    modify condition of compile

--- app/services/queue_services/runner.rb
+++ app/services/queue_services/runner.rb
@@ -43,10 +43,7 @@ module QueueServices
 
       require 'open3'
 
-      task = Task.find(task_id)
-      if task.is_scoring then
-        Open3.capture3("g++ -O2 -std=c++14 /data/tasks/#{task_id}/judge.cpp -o /data/tasks/#{task_id}/judge.exe")
-      end
+      Open3.capture3("[ -f /data/tasks/#{task_id}/judge.cpp ] && g++ -O2 -std=c++14 /data/tasks/#{task_id}/judge.cpp -o /data/tasks/#{task_id}/judge.exe")
 
       cmd = "docker run -v /data/submissions/#{@submission.id}:/#{student_name} -v /data/tasks/#{task_id}:/#{task_title}:ro 'gcc:latest' bash -c \"bash /#{task_title}/test.sh #{student_name} #{task_title}\""
       o, e, s = Open3.capture3(cmd)
